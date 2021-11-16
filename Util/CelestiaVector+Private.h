2d23ee1e1cdcabd6f530b7852a6e8d3f27e0e9cc
Levin Li <lilinfeng303@outlook.com>
2021-11-16 21:43:43 +0800

    Update to reflect changes in upstream

--- Util/CelestiaVector+Private.h
+++ Util/CelestiaVector+Private.h
@@ -10,7 +10,6 @@
 //
 
 #import "CelestiaVector.h"
-#include <celmath/vecmath.h>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
@@ -26,30 +25,5 @@
 - (Eigen::Vector3d)vector3d;
 - (Eigen::Quaternionf)quaternionf;
 - (Eigen::Quaterniond)quaterniond;
-
-+ (CelestiaVector*)vectorWithVec2f:(Vec2f)v;
-+ (CelestiaVector*)vectorWithVec3f:(Vec3f)v;
-+ (CelestiaVector*)vectorWithVec4f:(Vec4f)v;
-+ (CelestiaVector*)vectorWithVec3d:(Vec3d)v;
-+ (CelestiaVector*)vectorWithVec4d:(Vec4d)v;
-+ (CelestiaVector*)vectorWithPoint2f:(Point2f)v;
-+ (CelestiaVector*)vectorWithPoint3f:(Point3f)v;
-+ (CelestiaVector*)vectorWithPoint3d:(Point3d)v;
-- (CelestiaVector*)initWithVec2f:(Vec2f)v;
-- (CelestiaVector*)initWithVec3f:(Vec3f)v;
-- (CelestiaVector*)initWithVec4f:(Vec4f)v;
-- (CelestiaVector*)initWithVec3d:(Vec3d)v;
-- (CelestiaVector*)initWithVec4d:(Vec4d)v;
-- (CelestiaVector*)initWithPoint2f:(Point2f)v;
-- (CelestiaVector*)initWithPoint3f:(Point3f)v;
-- (CelestiaVector*)initWithPoint3d:(Point3d)v;
-- (Point2f)point2f;
-- (Point3f)point3f;
-- (Point3d)point3d;
-- (Vec2f)vec2f;
-- (Vec3f)vec3f;
-- (Vec4f)vec4f;
-- (Vec3d)vec3d;
-- (Vec4d)vec4d;
 
 @end
