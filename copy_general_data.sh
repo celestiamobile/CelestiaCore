60f6b8610ce41c5389a3b05605310a2083470542
Levin Li <lilinfeng303@outlook.com>
2024-03-19 23:41:36 +0800

    Fix celestia.cfg

--- copy_general_data.sh
+++ copy_general_data.sh
@@ -27,13 +27,18 @@ for directory in 'data' 'extras' 'extras-standard' 'models' 'textures' 'warp';do
     fi
 done
 
-for file in "celestia.cfg" "controls.txt" "demo.cel" "guide.cel" "start.cel" "COPYING" "AUTHORS" "TRANSLATORS";do
+for file in "controls.txt" "demo.cel" "guide.cel" "start.cel" "COPYING" "AUTHORS" "TRANSLATORS";do
     f=$CELESTIA_REPO_ROOT/$file
     if [ $f -nt $CELESTIA_ROOT/$file ];then
         echo "cp $f $CELESTIA_ROOT/$file"
         cp $f $CELESTIA_ROOT/$file
     fi
 done
+
+cp $CELESTIA_REPO_ROOT/celestia.cfg.in $CELESTIA_ROOT/celestia.cfg
+TO_REPLACE="@CELCFG_EXTRAS_DIRS@"
+NEW_STRING=""
+sed -i '' -e "s#${TO_REPLACE}#${NEW_STRING}#g" $CELESTIA_ROOT/celestia.cfg
 
 if [ $DIDCOPY -eq 1 ];then
     echo "Touch $BUILT_PRODUCTS_DIR/$PRODUCT_NAME.app"
