16778c2d6e766e1489d5769a356ace3282dbb45b
Levin Li <lilinfeng303@outlook.com>
2024-11-26 22:25:00 -0800

    Update according to upstream

--- App/CelestiaUniverse.mm
+++ App/CelestiaUniverse.mm
@@ -1,15 +1,15 @@
 //
 // CelestiaUniverse.mm
 //
-// Copyright © 2020 Celestia Development Team. All rights reserved.
+// Copyright (C) 2020-present, Celestia Development Team
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-//
 
 #include <celengine/body.h>
+#include <celengine/location.h>
 #import "CelestiaUniverse.h"
 #import "CelestiaSelection+Private.h"
 #import "CelestiaUniverse+Private.h"
